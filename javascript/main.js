5a0ceb4427fa5c282617ab5e5c7f79bfe2bd96f5
Jeffrey Graves <jagraves21@students.tntech.edu>
2021-09-01 18:20:49 -0500

    updated javascript file

--- javascript/main.js
+++ javascript/main.js
@@ -1,33 +1,55 @@
-window.onload = function() {
-	var top_nav = document.getElementById("top-nav");
-	var top_nav_hamburger = document.getElementById("top-nav-hamburger");
-	if (top_nav != null && top_nav_hamburger != null) {
-		top_nav.classList.add("responsive");
+function get_hamburger_button() {
+	var inner_span = document.createElement("span");
+	inner_span.classList.add("hamburger-inner");
+	
+	var outer_span = document.createElement("span");
+	outer_span.classList.add("hamburger-box");
+	outer_span.appendChild(inner_span);
+	
+	var button = document.createElement("button")
+	button.classList.add("hamburger");
+	button.classList.add("hamburger--squeeze");
+	button.type = "button";
+	button.appendChild(outer_span);
+	
+	return button;
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+	console.log("HERE")
+	var top_nav = document.getElementById("top_nav");
+	var top_nav_logo = document.getElementById("top_nav_logo");
+	var top_nav_links = document.getElementById("top_nav_links");
+	if (top_nav != null && top_nav_links != null && top_nav_logo != null) {
+		var top_nav_hamburger = get_hamburger_button();
+		top_nav_hamburger.id = "top_nav_hamburger"; 
 		top_nav_hamburger.addEventListener("click", function() {
-			top_nav.classList.toggle("active");
+			top_nav_links.classList.toggle("hidden");
 		});
+		top_nav_links.classList.add("hidden");
+		top_nav.classList.add("has-hamburger");
+		top_nav_logo.appendChild(top_nav_hamburger);
 	}
 
 	var hamburgers = document.querySelectorAll(".hamburger");
 	for (var ii = 0; ii < hamburgers.length; ii++) {
 		hamburgers.item(ii).addEventListener("click", function() {
-			console.log(hamburgers.item(ii) + " clicked");
 			this.classList.toggle("is-active");
 		});
 	}
-};
+});
 
-window.onscroll = function() {sticky_top_nav()};
-function sticky_top_nav() {
-	var top_nav = document.getElementById("top-nav");
-	var header = document.getElementById("page-header").getElementsByTagName("h1")[0];
-	var sticky = Math.max(0, header.offsetTop - top_nav.offsetHeight);
-	console.log(window.pageYOffset);
-	if (window.pageYOffset > sticky) {
-		//console.log("add " + window.pageYOffset + " " + sticky + " " + (window.pageYOffset > sticky));
-		top_nav.classList.add("sticky");
-	} else {
-		//console.log("remove " + window.pageYOffset + " " + sticky + " " + (window.pageYOffset > sticky));
-		top_nav.classList.remove("sticky");
+
+window.onscroll = function() {
+	var top_nav = document.getElementById("top_nav");
+	var page_header_title = document.getElementById("top_nav_logo");
+	if (top_nav != null && page_header_title != null) {
+		var sticky = Math.max(0, page_header_title.offsetTop - top_nav.offsetHeight);
+		if (window.pageYOffset > sticky) {
+			top_nav.classList.add("sticky");
+		} else {
+			top_nav.classList.remove("sticky");
+
+		}
 	}
 }
